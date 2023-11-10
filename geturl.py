794427ca886ce92fa4ef0e4fccc0748932739181
WaiCat <rxg1123@gmail.com>
2023-11-10 17:05:53 +0900

    change_class

--- geturl.py
+++ geturl.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 # 웹 페이지의 URL 설정
 url = 'https://young.busan.go.kr/index.nm'
 
+
+
 # 웹 페이지의 내용을 가져오기
 response = requests.get(url)
 
@@ -38,7 +40,7 @@ if response.status_code == 200:
                 soup1 = BeautifulSoup(response1.text, "html.parser")
 
                 # 3. 원하는 텍스트 추출
-                element = soup1.find("div", class_="sub_content")
+                element = soup1.find("div", class_="cont_body")
                 if element:
                     text = element.text.replace("\n", "")  # 토큰 줄이기 위한 개행 제거
                     print(text)
